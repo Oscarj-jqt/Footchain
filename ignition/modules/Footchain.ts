b889a91cb22518d3568e59e82035f478b68895fc
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-03-06 13:04:29 +0100

    FootchainModule deployed successfully

--- ignition/modules/Footchain.ts
+++ ignition/modules/Footchain.ts
@@ -3,11 +3,11 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const footchainModule = buildModule("footchainModule", (m) => {
 
   //addresse Ethereum valide
-  const param1 = "0xMyAddress";
-  const param2 = m.getParameter("initialValue", 100);
+  const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+  const initialValue = m.getParameter("initialValue", "100");
 
 
-  const footchain = m.contract("Footchain", [param1, param2]);
+  const footchain = m.contract("Footchain", [ownerAddress, initialValue]);
 
   return { footchain };
 });
