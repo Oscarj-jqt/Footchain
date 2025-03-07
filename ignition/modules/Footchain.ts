8568e8762df60916c6ed506a4953ca529a3cf800
Bagharia <hu.alexis@hotmail.com>
2025-03-07 13:54:50 +0100

    front

--- ignition/modules/Footchain.ts
+++ ignition/modules/Footchain.ts
@@ -1,14 +1,15 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-
 const footchainModule = buildModule("footchainModule", (m) => {
-  // Déploiement du smart contrat Footchain
-  const footchain = m.contract("Footchain");
+  // Définir les arguments nécessaires pour le constructeur
+  const name = "Footchain"; // Le nom du token
+  const symbol = "FOOT"; // Le symbole du token
+
+  // Déployer le contrat avec ces arguments
+  const footchain = m.contract("Footchain", [name, symbol]);
 
   return { footchain };
 });
 
 export default footchainModule;
+
